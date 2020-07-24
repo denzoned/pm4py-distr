e1127bde702ce2056c4f2aa1f3af91020d5afc62
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-07-24 14:08:51 +0200

    updated script

--- ssh_connection.py
+++ ssh_connection.py
@@ -10,18 +10,35 @@ def get_ssh_client(server, username, password):
 password = input("Insert password: ")
 script = input("script ?")
 
-ssh1 = get_ssh_client("137.226.117.71", "berti", password)
-ssh2 = get_ssh_client("137.226.117.72", "berti", password)
-ssh3 = get_ssh_client("137.226.117.73", "berti", password)
-ssh4 = get_ssh_client("137.226.117.74", "berti", password)
-ssh5 = get_ssh_client("137.226.117.75", "berti", password)
-#ssh6 = get_ssh_client("137.226.117.76", "berti", password)
+ENABLE_SLAVE_1 = True
+ENABLE_SLAVE_2 = True
+ENABLE_SLAVE_3 = True
+ENABLE_SLAVE_4 = True
+ENABLE_SLAVE_5 = True
+ENABLE_SLAVE_6 = False
 
-ssh_stdin, ssh_stdout, ssh_stderr = ssh1.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh2.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh3.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh4.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh5.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
-#ssh_stdin, ssh_stdout, ssh_stderr = ssh6.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+if ENABLE_SLAVE_1:
+    ssh1 = get_ssh_client("137.226.117.71", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh1.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+
+if ENABLE_SLAVE_2:
+    ssh2 = get_ssh_client("137.226.117.72", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh2.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+
+if ENABLE_SLAVE_3:
+    ssh3 = get_ssh_client("137.226.117.73", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh3.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+
+if ENABLE_SLAVE_4:
+    ssh4 = get_ssh_client("137.226.117.74", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh4.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+
+if ENABLE_SLAVE_5:
+    ssh5 = get_ssh_client("137.226.117.75", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh5.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
+
+if ENABLE_SLAVE_6:
+    ssh6 = get_ssh_client("137.226.117.76", "berti", password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh6.exec_command("cd /home/berti/pm4py-distr && echo "+password+" | sudo -S bash "+script)
 
 time.sleep(500)
